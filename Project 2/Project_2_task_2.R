629f647711bb267bb53ac9f7ec1df1bd28fd617b
Helge Bergo <helge.bergo@gmail.com>
2019-11-08 16:05:33 +0100

    Task 2 now works!

--- Project 2/Project_2_task_2.R
+++ Project 2/Project_2_task_2.R
@@ -4,22 +4,23 @@
 
 ## Problem 2 - Calibrating Climate Models
 #rm(list = ls()) # clear environment and variables
-library(Matrix)
+#library(Matrix)
 
 # Given values
-theta <- seq(0.25, 0.50, by=0.005) 
-mu <- 0.5 # mean vector to n
-sigma <- 0.5 # variance squared?
+theta <- seq(0.25, 0.50, by=0.005)# mu_A
+sigma <- 0.5^2 # variance squared
 phi_m <- 15 # Matern type correlation function parameter
 
 # Conditional values 
 theta_conditional = c(0.3, 0.35, 0.39, 0.41, 0.45) # mu_B
-mu <- theta_conditional
-#mu <- theta
 Y_conditional = c(0.5, 0.32, 0.40, 0.35, 0.60) # x_B
-# theta_conditional = c(0.3, 0.35, 0.39, 0.41, 0.45, 0.33) # mu_B with extra conditional parameter
-# Y_conditional = c(0.5, 0.32, 0.40, 0.35, 0.60, 0.40) # x_B
+
+# Extra measurement:
+#theta_conditional <- c(theta_conditional, 0.33) # mu_B with extra conditional parameter
+#Y_conditional <- c(Y_conditional, 0.40) # x_B
 #theta <- setdiff(theta, theta_conditional); # remove the steps where we have a conditional value
+
+
 
 # Build distance matrices H
 ones_A <- as.matrix(rep(1.0, length(theta)));
@@ -41,92 +42,53 @@ Sigma_AB <- sigma * (1 + phi_m * H_AB) * exp(-phi_m * H_AB)
 
 
 # Calculate the expected value
-Y <- mu + Sigma_AB %*% solve(Sigma_B) %*% (Y_conditional - theta_conditional) 
+Y <- theta + Sigma_AB %*% solve(Sigma_B) %*% (Y_conditional - theta_conditional) 
+
 
 
 # Calculating 90% confidence interval
 Var <- Sigma_A - Sigma_AB %*% solve(Sigma_B) %*% t(Sigma_AB)
-#Var[Var < 0] <- 0 # Remove negative values 
+diag(Var)[diag(Var) < 0] <- 0 # Remove negative values from floating point errors
 z <- 1.64
 upper <- Y + z*sqrt(diag(Var))
 lower <- Y - z*sqrt(diag(Var))
 
-# Plotting
-#require(plotrix) # Solution with plotrix
-#plotCI(theta, Y, ui=upper, li=lower)
-
- 
-# df <- data.frame(theta,Y,upper, lower)
-# require(ggplot2) # Solution with ggplot
-# ggplot(df, aes(x = theta, y = Y)) +
-#   geom_point(size = 2) +
-#   geom_errorbar(aes(ymax = upper, ymin = lower))
 
 
 # Plot with error and conditional values
-plot(NULL,NULL, xlim = c(0.25,0.5), ylim = c(0.1, 1.5), 
+pdf("plot5.pdf") 
+plot(NULL,NULL, xlim = c(0.25,0.5), ylim = c(0.2, 1.0), 
      xlab = expression(paste(theta)), ylab = expression(paste(y(theta))), cex.lab = 1.5)
 lines(theta,Y,col="black")
 lines(theta,upper,lty=2)
 lines(theta,lower,lty=2)
 points(theta_conditional,Y_conditional,col = "red", pch = 19)
-legend(0.35,1.2,legend = c("Model","Bounds", "Reality"),
+legend(0.35,0.9,legend = c("Model","Bounds", "Measurements"),
        col = c("black","black","red"),cex = 0.8, lty = c(1,2,NA), pch = c(NA,NA,19))
-
+dev.off()
 
 
 ## 2.b)
 
 # Calculating the treshold 
 treshold = 0.30
-p <- pnorm(treshold, mean = Y, sd = diag(Var), lower.tail = TRUE)
+p <- pnorm((treshold - Y)/sqrt(diag(Var)), lower.tail = TRUE)
 
-# plot(NULL, NULL, xlim = c(0.25,0.50), ylim = c(0, 0.002), xlab = "Theta", 
-#      ylab = ('p(x[A] < 0.3 | x[B])'), cex.lab = 1.5, cex.axis = 1.5)
-# points(theta,p)
+
+pdf("treshold5.pdf") 
+plot(NULL, NULL, xlim = c(0.25,0.50), ylim = c(0.0, 0.2), xlab = "Theta",
+     ylab = (expression(paste("p(x"[A],"<0.3|x"[B],")"))), cex.lab = 1.5, cex.axis = 1.5)
+lines(theta,p)
+dev.off()
+
+best_guess <- theta[match(max(p[5:50]),p)]
+print("Max value: ")
+print(best_guess)
+
+
 
 ## 2.c)
 
 
 
-# -----------
-
-### Test plotting
-
-# # Test to see how much we miss the conditional values
-# plot(NULL,NULL, xlim = c(0.25,0.5), ylim = c(0.3, 0.8), 
-#      xlab = "Theta", ylab = "y(theta)", cex.lab = 1.5)
-# lines(theta,Y,col="blue")
-# points(theta_conditional,Y_conditional,col = "red")
-# legend(0.35,0.8,legend = c("Model", "Reality"),
-#        col = c("blue","red"),cex = 0.8,lty=1:3)
-
-
-# # 
-# # # H_AB
-# plot(NULL, NULL, xlim = c(0,50), ylim = c(0, 0.2), 
-# xlab = "Index", ylab = "Correlation", cex.lab = 1.5, cex.axis = 1.5)
-# for(i in 1:length(theta_conditional)){
-#   lines(H_AB[,i])
-# }
-# 
-# # H_A
-# plot(NULL, NULL, xlim = c(0,50), ylim = c(0, 0.2), 
-# xlab = "Index", ylab = "Correlation", cex.lab = 1.5, cex.axis = 1.5)
-# for(i in 1:length(theta)){
-#   lines(H_A[,i])
-# }
-# # H_B
-# plot(NULL, NULL, xlim = c(0,50), ylim = c(0, 0.2), 
-# xlab = "Index", ylab = "Correlation", cex.lab = 1.5, cex.axis = 1.5)
-# for(i in 1:length(theta_conditional)){
-#   lines(H_B[,i])
-# }
-# 
-# # Sigma_AB
-# plot(NULL, NULL, xlim = c(0,50), ylim = c(0.05, 0.25), 
-# xlab = "Index", ylab = "Correlation", cex.lab = 1.5, cex.axis = 1.5)
-# for(i in 1:length(theta_conditional)){
-#   lines(Sigma_AB[,i])
-# }
 
